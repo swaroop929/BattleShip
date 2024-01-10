664a5eb47bcc10266e03b73763faf95e70ea04a4
Sai Swaroop Maram <49112390+swaroop929@users.noreply.github.com>
2024-01-10 14:55:13 +0530

    Fixed bugs in ShotResult() in OpponentGrid.cpp

--- myCode/OpponentGrid.cpp
+++ myCode/OpponentGrid.cpp
@@ -26,7 +26,6 @@ void OpponentGrid::shotResult(const Shot &shot, Shot::Impact impact) {
 
 	shots.insert( { shot.getTargetPosition(), impact });
 
-
 	if (impact == Shot::Impact::HIT) {
 		ShotsHit.insert(shot.getTargetPosition());
 	}
@@ -38,49 +37,45 @@ void OpponentGrid::shotResult(const Shot &shot, Shot::Impact impact) {
 	int dir_col[4] = { -1, 1, 0, 0 }; //L, R, U, D
 
 	char PrevRow = ShotUnderConsideration.getRow();
-	char NextRow = ShotUnderConsideration.getRow();
+	char NextRow = PrevRow;
 	int PrevCol = ShotUnderConsideration.getColumn();
-	int NextCol = ShotUnderConsideration.getColumn();
+	int NextCol = PrevCol;
 
 	if (impact == Shot::Impact::SUNKEN) {
-		SunkenShipOccupArea.insert(shot.getTargetPosition());
+		SunkenShipOccupArea.insert(shot.getTargetPosition()); //A2
 		//for sure one of the grid in interested ship
 		for (int itr = 0; itr < 4; itr++) {
-			for (auto MatchinHits : ShotsHit) { //search in whole array for A4
-				/*PrevRow = NextRow; //A
-				 PrevCol = NextCol; //5*/
-				if (GridPosition(PrevRow + dir_row[itr], PrevCol + dir_col[itr])
-						== MatchinHits) {
-					int correct_dir = itr; //left ani telsindi
-					for (auto MatchinHitss : ShotsHit) {
-						NextRow = PrevRow + dir_row[correct_dir]; //A
-						NextCol = PrevCol + dir_col[correct_dir]; //4
-						for (auto MatchinHits_itr : ShotsHit) {
-							if (GridPosition(NextRow, NextCol)
-									== MatchinHits_itr) {
-								if (NextRow >= 'A'
-										&& NextRow <= (char) ('A' + rows - 1)
-										&& NextCol >= 1 && NextCol <= columns) {
-									SunkenShipOccupArea.insert(MatchinHits_itr);
-									PrevCol = NextCol;
-									PrevRow = NextRow;
-								}
-							}
-						}
-					}
+			NextRow = PrevRow + dir_row[itr]; //A1
+			NextCol = PrevCol + dir_col[itr];
+			if (NextRow >= 'A' && NextRow <= ('A' + rows - 1) && NextCol >= 1
+					&& NextCol <= columns) {
+				auto pos = ShotsHit.find(GridPosition(NextRow, NextCol));
+				if (pos != ShotsHit.end()) {
+					SunkenShipOccupArea.insert(*pos);
+					//continue in that direction
+					set<GridPosition>::iterator pos2;
+					do {
+						NextRow = NextRow + dir_row[itr];
+						NextCol = NextCol + dir_col[itr];
+						pos2 = ShotsHit.find(
+								GridPosition(NextRow, NextCol));
+						if (pos2 != ShotsHit.end())
+							SunkenShipOccupArea.insert(*pos2);
+					} while (pos2 != ShotsHit.end());
+
 
 				}
 			}
-
 		}
+
 		sunkenShips.push_back(
 				Ship(*(SunkenShipOccupArea.begin()),
 						*(SunkenShipOccupArea.rbegin())));
 	}
 
-	/*for(auto grid:SunkenShipOccupArea){
-	 cout<<grid.getRow()<<grid.getColumn()<<endl;
-	 }*/
+	/*for (auto grid : SunkenShipOccupArea) {
+		cout << grid.getRow() << grid.getColumn() << endl;
+	}*/
 }
 
 vector<Ship> OpponentGrid::getSunkenShips() const {
